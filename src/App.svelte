eba89e52186ce628a629a9e919beb937384fb0e6
Yimin Sun <56568926+CodeYimin@users.noreply.github.com>
2022-08-13 21:59:33 -0400

    Change to sveltekit (starter template)

--- src/App.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-  import KanaTyper from "./pages/kana_typer/KanaTyper.svelte";
-</script>
-
-<KanaTyper />
--- src/utils/types.ts
+++
@@ -1,5 +0,0 @@
-export type EventWithTarget<T extends EventTarget, E extends Event = Event> = E & {
-  currentTarget: T;
-};
-
-export type EventHandler<T extends EventTarget, E extends Event = Event> = (event: EventWithTarget<T, E>) => any;
